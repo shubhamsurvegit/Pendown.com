1d67684858e716809f7dd0a848dc183856b8ffec
shubham <surveshubham0@gmail.com>
2023-08-09 18:12:09 +0530

    d4

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ app.use('/styles',express.static('styles'))
 app.use(session({secret: "Shh, its a secret!",resave:false}));
 
 const url="mongodb://localhost:27017/article/";
-const a= 'test2456'
+const a= 'test24567'
 
 mongoose.connect(url,{useNewUrlParser:true})
 .then(()=>console.log("mongo db connected"))
